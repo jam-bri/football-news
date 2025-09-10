e9474e5a6aa62f7a075e28869b5135cf7b535f12
jam-bri <briana.yadjam@etu.imt-nord-europe.fr>
2025-09-10 13:55:01 +0200

    finished tutorial 2

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_news, show_news, show_xml
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-news/', create_news, name='create_news'),
+    path('news/<str:id>/', show_news, name='show_news'),
+    path('xml/', show_xml, name='show_xml'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import NewsForm
+from main.models import News
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
+    news_list = News.objects.all()
+
     context = {
         'npm' : '2506561555',
         'name': 'Berguegou Briana Yadjam',
-        'class': 'PBP KI'
+        'class': 'PBP KI',
+        'news_list': news_list
     }
 
     return render(request, "main.html", context)
+
+def create_news(request):
+    form = NewsForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_news.html", context)
+
+def show_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.increment_views()
+
+    context = {
+        'news': news
+    }
+
+    return render(request, "news_detail.html", context)
+
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize("xml", news_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
