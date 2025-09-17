97ee7630804b5eeb0905b5b943f209c53ab023a8
jam-bri <briana.yadjam@etu.imt-nord-europe.fr>
2025-09-17 09:27:35 +0200

    tutorial 3 completed

--- football_news/settings.py
+++ football_news/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
